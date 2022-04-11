fa5f6d6c7ce8771aa49c923cca57ced5f651e745
Munde-J <mundeangelah@gmail.com>
2022-04-11 06:26:22 +0300

    first commit

--- src/main.kt
+++ src/main.kt
@@ -17,9 +17,9 @@ fun main(){
 
   var pack = Person("Pack",23,2,59)
   var cndy = Person("Cndy",20,1,57)
-    //var addPerson= mutableListOf(pack,cndy)
+    var addPerson= mutableListOf(pack,cndy)
       //  println(p.plus(addPerson))
- var addPerson=(Person2(listOf(pack,cndy)))
+//var addPerson=(Person2(listOf(pack,cndy)))
      println(p.plus(addPerson))
 
 
